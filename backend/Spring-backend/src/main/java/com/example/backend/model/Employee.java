a62ff7667db1a361be6a12300ce6e74fc45f3499
trayoub <traikiayoub@gmail.com>
2022-05-21 20:49:03 +0100

    crud employee done

---
+++ backend/Spring-backend/src/main/java/com/example/backend/model/Employee.java
@@ -0,0 +1,41 @@
+package com.example.backend.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Data @Table(name="Employees")
+public class Employee {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name="first_name",nullable=false)
+	private String firstname;
+	@Column(name="last_name",nullable=false)
+	private String lastname;
+	@Column(name="email",nullable=false)
+	private String email;
+	@Column(name="fuction",nullable=false)
+	private String function;
+	@Column(name="salary",nullable=false)
+	private double salary;
+	@Column(name="grade",nullable=false)
+	private int grade;
+	@Temporal(TemporalType.TIMESTAMP) @CreationTimestamp @Column(name="Date_Creation",nullable=false)
+    private Date DateCreation;
+
+	
+}
